36a41ff11c31512fdcc88d7800195555dc2ca3e1
paulego <egopaul@inbox.ru>
2022-09-06 15:07:04 +0500

    Removed secret data in .env

--- project/settings.py
+++ project/settings.py
@@ -1,19 +1,23 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': os.getenv('ENGINE'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
-INSTALLED_APPS = ['datacenter']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-SECRET_KEY = 'REPLACE_ME'
+INSTALLED_APPS = ['datacenter']
 
 DEBUG = True
 
@@ -21,8 +25,8 @@ ROOT_URLCONF = 'project.urls'
 
 ALLOWED_HOSTS = ['*']
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -30,7 +34,6 @@ TEMPLATES = [
         'APP_DIRS': True,
     },
 ]
-
 
 USE_L10N = True
 
